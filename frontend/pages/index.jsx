8439dec861e9961eef593208a928d94b4f866c27
Gerald <geraldpng@gmail.com>
2020-06-22 20:58:55 +0800

    Add title

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import Head from 'next/head';
 // components
 import DashboardLayout from '../components/Layouts/DashboardLayout';
 import Dashboard from '../components/Dashboard/Dashboard';
 
 const Index = () => {
   return (
-    <DashboardLayout>
-      <Dashboard />
-    </DashboardLayout>
+    <>
+      <Head>
+        <title>HR Employee Management</title>
+      </Head>
+      <DashboardLayout>
+        <Dashboard />
+      </DashboardLayout>
+    </>
   );
 };
 
